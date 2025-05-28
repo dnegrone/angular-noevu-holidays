11d48ba5e8aa31cf62db0cc21775859e048b38fa
Alexander Costa <alexcesar@alexcesar.com>
2025-05-28 12:51:52 -0300

    holiday card added

---
+++ src/app/shared/component/holiday-card/holiday-card/holiday-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-holiday-card',
+  imports: [],
+  templateUrl: './holiday-card.component.html',
+  styleUrl: './holiday-card.component.scss'
+})
+export class HolidayCardComponent {
+
+}
